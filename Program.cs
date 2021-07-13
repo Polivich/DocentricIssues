bd2d0c0a4412b9c1a1f1fe1f7a23a9be91b821d8
Polivich <35722791+Polivich@users.noreply.github.com>
2021-07-13 12:36:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoinDocFileIssue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start processing...");
+
+            string templatePath = Path.Combine(Environment.CurrentDirectory, "Templates", "main.docx");
+            string outputDir = ConfigurationManager.AppSettings.Get("OutputDirectoryName");
+            string outputFileName = ConfigurationManager.AppSettings.Get("OutputFileName");
+            string outputPath = Path.Combine(Environment.CurrentDirectory, outputDir, outputFileName);
+
+            try
+            {
+                //do file generation
+                ReportService.GenerateReport(templatePath, outputPath, new { });
+
+                Console.WriteLine("File generated successfully!");
+                Console.WriteLine($"Path: {outputPath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+            }
+
+            Console.WriteLine();
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReportService.cs
@@ -0,0 +1,43 @@
+﻿using Docentric.Documents.Reporting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace JoinDocFileIssue
+{
+    public static class ReportService
+    {
+        public static void GenerateReport(string inputTemplatePath, string outputPath, object data)
+        {
+            CreateDirectory(outputPath);
+
+            using (Stream templateStream = File.OpenRead(inputTemplatePath))
+            using (Stream reportDocumentStream = File.Create(outputPath))
+            {
+                var dg = new DocumentGenerator(null);
+
+                dg.WriteErrorsAndWarningsToDocument = false;
+                dg.SetDefaultDataSourceValue(data);
+                
+                //generate Docentric document
+                var result = dg.GenerateDocument(templateStream, reportDocumentStream);
+
+                if (result.HasErrors)
+                {
+                    IEnumerable<string> errorMessages = result.Errors.Select(x => $"Severity: {x.Severity.ToString()}; Message: {x.Message}");
+                    throw new Exception(string.Join(Environment.NewLine, errorMessages));
+                }
+            }
+        }
+
+        #region -- Private Methods --
+        private static void CreateDirectory(string path)
+        {
+            string dirPath = Path.GetDirectoryName(path);
+            if (!Directory.Exists(dirPath))
+                Directory.CreateDirectory(dirPath);
+        }
+        #endregion -- Private Methods --
+    }
+}
